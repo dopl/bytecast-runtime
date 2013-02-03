69e0b2f527990a3cf5dda782960aed4db73cf57b
TongXu <Your email>
2013-02-03 07:12:14 -0500

    tongxu firstly create

---
+++ src/TongXuJNITest/TestJNI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TongXuJNITest;
+
+/**
+ *
+ * @author tongxu
+ */
+
+public class TestJNI {
+    
+    static{
+        try{
+                System.loadLibrary("hello");
+        }
+        catch(UnsatisfiedLinkError e){
+            System.err.println("Cannot load hello library:\n" + e.toString());
+        }
+            
+    }
+
+    public native void SayHello(String strName);
+    
+    
+}
