ca3e00fdc40fef638d4ebebe6f43962e144a3c25
Cheng Xu <cxu09@syr.edu>
2013-03-02 17:06:39 -0500

    printf mabi

---
+++ asm_tests/test2.c
@@ -0,0 +1,7 @@
+ #include <stdio.h>
+
+int main(int argc, char * argv[]){
+  char str[1];
+ 
+  return 0;
+}
---
+++ asm_tests/test3.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main(int argc, char * argv[]){
+   
+  int a=10;
+  int  b=20;
+  char c[1000]="aaaaaaaaa";
+  char d='d';
+  
+  printf(a,b,c,d);
+
+  return 0;
+}
