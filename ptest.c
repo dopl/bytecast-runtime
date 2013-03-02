eea1cad58fb2f99523526adf01ba19a3f5bc5064
Mengxi Chen <txu04@syr.edu>
2013-03-02 15:04:58 -0500

    add ptest.c

---
+++ ptest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+            int ret = 0;
+            int ret2 = 0;
+            char str[] = "abc";
+            printf("the pure printout");
+            printf("the int si%d",ret);
+printf("the string is %s",str);
+printf("the string is %s%d",str,ret2);
+printf("The value is %d%d\n",ret,ret2);
+printf("the value is %d%d%s",ret,ret2,str);
+return 0;
+
+}
