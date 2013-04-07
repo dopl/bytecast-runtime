d396eb80bd2d9f372143b83259677942cad5a0fd
Mengxi Chen <mchen40@syr.edu>
2013-04-07 17:56:34 -0400

    Folder reorganize

--- asm_tests/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char * argv[]){
-
-  char str[1];
-  str[0] = 'c';
-  printf(str);
-  return 0;
-}
--- asm_tests/test2.c
+++
@@ -1,7 +0,0 @@
- #include <stdio.h>
-
-int main(int argc, char * argv[]){
-  char str[1];
- 
-  return 0;
-}
